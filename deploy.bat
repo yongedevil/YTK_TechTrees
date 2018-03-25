b5564eddcc3d24f85ab4287c36cd35c338ef54e9
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-03-24 21:44:33 -0400

    Updated for 1.4.1

--- deploy.bat
+++ deploy.bat
@@ -1,15 +1,12 @@
 
 rem @echo off
 
-set H=R:\KSP_1.3.1_dev
+set H=R:\KSP_1.4.1_dev
 set GAMEDIR=YongeTech_TechTreesPlugin
 
 echo %H%
 
 copy /Y "%1%2" "GameData\%GAMEDIR%\Plugins"
-rem copy /Y %GAMEDIR%.version GameData\%GAMEDIR%
-
 mkdir "%H%\GameData\%GAMEDIR%"
 xcopy  /E /y GameData\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
 
-pause
